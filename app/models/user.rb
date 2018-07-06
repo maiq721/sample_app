fa423cff120fafeb91cb470c487c1e451305cf99
maiq721 <maiq721@gmail.com>
2018-07-06 16:16:01 +0700

    Make basic User model

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  before_save {self.email = email.downcase}
+  validates :name, presence: true, length: {maximun: Settings.validate.name.maximum}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: Settings.validate.email.maximum},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, presence: true, length: {minimum: Settings.validate.password.minimum}
+end
