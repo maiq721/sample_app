9aca83830ecd68b46ed3d2c4444f95a440a9edcc
maiq721 <maiq721@gmail.com>
2018-07-17 14:41:44 +0700

    Last Chap

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
     return unless logged_in?
-      @micropost  = current_user.microposts.build
-      @feed_items = current_user.feed.by_date.page(params[:page]).per Settings.show_limit.show_10
+    @micropost  = current_user.microposts.build
+    @feed_items = current_user.feed.by_date.page(params[:page]).per Settings.show_limit.show_10
   end
 
   def help; end
